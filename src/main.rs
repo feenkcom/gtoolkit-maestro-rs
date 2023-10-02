cb6a33ae4e7a2e0669ed00eeac2d5b36efc8214e
Andrei Chiș <chisvasileandrei@gmail.com>
2023-10-02 12:53:46 +0200

    Switch to Pharo 10

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ pub const DEFAULT_IMAGE_NAME: &str = "GlamorousToolkit";
 pub const DEFAULT_IMAGE_EXTENSION: &str = "image";
 
 pub const DEFAULT_PHARO_IMAGE: &str =
-    "https://dl.feenk.com/pharo/Pharo11-SNAPSHOT.build.707.sha.f720787.arch.64bit.zip";
+    "https://dl.feenk.com/pharo/Pharo10-SNAPSHOT.build.538.sha.179ef65.arch.64bit.zip";
 
 pub const SERIALIZATION_FILE: &str = "gtoolkit.yaml";
 pub const DOCKERFILE: &str = "scripts/docker/gtoolkit/Dockerfile";
