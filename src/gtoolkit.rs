7931e377abfcb884ddb43efc82acafcc518ab3b9
Andrei Chiș <chisvasileandrei@gmail.com>
2023-12-14 18:25:25 +0100

    Back to GtGtoolkitArchitecturalReport

--- src/gtoolkit.rs
+++ src/gtoolkit.rs
@@ -116,7 +116,7 @@ impl<'application> GToolkit for Smalltalk<'application> {
 
     fn run_architectural_report(&self) -> Result<()> {
         SmalltalkCommand::new("gtexportreport")
-            .arg("--report=GtGtoolkitArchitecturalDevelopmentReport")
+            .arg("--report=GtGtoolkitArchitecturalReport")
             .execute(&self.evaluator())
     }
 }
