f594f5a66c646fc7b0643fdbf6dc1ef3672ebe6f
Don Roberts <roberts@refactory.com>
2024-05-23 08:56:22 -0500

    Always overwrite installer executable

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -14,45 +14,38 @@ if [ $# -eq 0 ]
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  if [ ! -f "$installer" ]; then
-    arch_name="$(uname -m)"
-    if [ "${arch_name}" = "x86_64" ]; then
-      curl -o "$installer" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-x86_64-unknown-linux-gnu" > /dev/null
-    elif [ "${arch_name}" = "aarch64" ]; then
-      curl -o "$installer" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-aarch64-unknown-linux-gnu" > /dev/null
-    else
-      echo "$arch_name architecture is unsupported. Please submit an issue at https://github.com/feenkcom/gtoolkit/issues".
-      exit 1
-    fi
+  arch_name="$(uname -m)"
+  if [ "${arch_name}" = "x86_64" ]; then
+    curl -o "$installer" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-x86_64-unknown-linux-gnu" > /dev/null
+  elif [ "${arch_name}" = "aarch64" ]; then
+    curl -o "$installer" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-aarch64-unknown-linux-gnu" > /dev/null
+  else
+    echo "$arch_name architecture is unsupported. Please submit an issue at https://github.com/feenkcom/gtoolkit/issues".
+    exit 1
   fi
   chmod +x "$installer"
   ./$installer $arguments
 elif [[ "$OSTYPE" == "darwin"* ]]; then
+  arch_name="$(uname -m)"
+  is_m1=false
+  if [ "${arch_name}" = "x86_64" ]; then
+      if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
+          is_m1=true
+      fi
+  elif [ "${arch_name}" = "arm64" ]; then
+      is_m1=true
+  fi
 
-  if [ ! -f "$installer" ]; then
-    arch_name="$(uname -m)"
-    is_m1=false
-    if [ "${arch_name}" = "x86_64" ]; then
-        if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
-            is_m1=true
-        fi
-    elif [ "${arch_name}" = "arm64" ]; then
-        is_m1=true
-    fi
-
-    if [[ "$is_m1" == true ]]; then
-      curl -o "$installer" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-aarch64-apple-darwin" > /dev/null
-    else
-      curl -o "$installer" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-x86_64-apple-darwin" > /dev/null
-    fi
+  if [[ "$is_m1" == true ]]; then
+    curl -o "$installer" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-aarch64-apple-darwin" > /dev/null
+  else
+    curl -o "$installer" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-x86_64-apple-darwin" > /dev/null
   fi
   chmod +x "$installer"
   ./$installer $arguments
 
 elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-  if [ ! -f "$installer.exe" ]; then
-    curl -o "$installer.exe" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-x86_64-pc-windows-msvc.exe" > /dev/null
-  fi
+  curl -o "$installer.exe" -L -s -S "https://github.com/feenkcom/gtoolkit-maestro-rs/releases/latest/download/$installer-x86_64-pc-windows-msvc.exe" > /dev/null
   chmod +x "$installer.exe"
   ./$installer.exe $arguments
 else
